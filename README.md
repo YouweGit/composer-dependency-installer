3473da40302b520b21deb2370463d1c98c35c6b4
Stefan Boonstra <s.boonstra@youweagency.com>
2025-02-27 12:18:12 +0100

    feat: support skipping package installation when upstream has out-of-date package already installed
    * Add support to skipping package installation upstream when
      a different version is already installed
    * Rewrite phpunit test class to use PHPUnit 12
    * Add test function to cover new functionality
    * Add usage reference to README.md

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # composer-dependency-installer
 
-PHP package for installing Composer's dependencies.
+PHP package for installing Composer's dependencies upstream.
+
+## Usage
+To install a package in upstream software requiring this package, instantiate a new instance and call the install() function.
+
+## Usage reference
+```php
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Foo;
+
+class Bar
+{
+    public function __construct(
+        private readonly DependencyInstaller $dependencyInstaller
+    ) {}
+    
+    public function installUpstreamRepository(): void
+    {
+        $this->dependencyInstaller->installRepository(
+            name: 'some/package',
+            type: 'composer',
+            url: 'mypackagedomain.dev'
+        );
+    }
+    
+    public function installUpstreamPackage(): void
+    {
+        $this->dependencyInstaller->installPackage(
+            name: 'some/package',
+            version: '^2.0.0',
+            dev: true, // Whether it should be a dev dependency (e.g. require-dev). Optional, defaults to true
+            updateDependencies: false, // Whether dependencies can be updated. Optional, defaults to true. When enabled, passes -W flag to composer
+            allowOverrideVersion: true // Whether version can be updated with new version when package is altready installed upstream. Optional, defaults to true.
+        )
+    }
+}
+```
--- tests/DependencyInstallerTest.php
+++ tests/DependencyInstallerTest.php
@@ -10,47 +10,44 @@ declare(strict_types=1);
 namespace Youwe\Composer\DependencyInstaller\Tests;
 
 use Composer\Json\JsonFile;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversMethod;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\MockObject\Exception;
 use Seld\JsonLint\ParsingException;
 use Youwe\Composer\DependencyInstaller\DependencyInstaller;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * @coversDefaultClass \Youwe\Composer\DependencyInstaller\DependencyInstaller
- */
+#[CoversClass(DependencyInstaller::class)]
+#[CoversMethod(DependencyInstaller::class, '__construct')]
+#[CoversMethod(DependencyInstaller::class, 'installPackage')]
+#[CoversMethod(DependencyInstaller::class, 'installRepository')]
 class DependencyInstallerTest extends TestCase
 {
-    /** @var string */
-    private static $directory = __DIR__ . DIRECTORY_SEPARATOR . 'tmp';
+    private static string $directory = __DIR__ . DIRECTORY_SEPARATOR . 'tmp';
+    private DependencyInstaller $dependencyInstaller;
 
     /**
-     * @return void
+     * @throws Exception
      */
-    public static function setUpBeforeClass()
+    protected function setUp(): void
     {
-        static::tearDownAfterClass();
-
         mkdir(static::$directory);
         chdir(static::$directory);
         file_put_contents('composer.json', '{}');
+
+        $this->updateComposerFileReference();
     }
 
-    /**
-     * @return void
-     */
-    public static function tearDownAfterClass()
+    protected function tearDown(): void
     {
         if (is_dir(static::$directory)) {
             static::rrmdir(static::$directory);
         }
     }
 
-    /**
-     * @param string $src
-     *
-     * @return void
-     */
-    private static function rrmdir(string $src)
+    private static function rrmdir(string $src): void
     {
         $dir = opendir($src);
         while (false !== ($file = readdir($dir))) {
@@ -67,45 +64,24 @@ class DependencyInstallerTest extends TestCase
         rmdir($src);
     }
 
-
-    /**
-     * @return DependencyInstaller
-     * @covers ::__construct
-     */
-    public function testConstructor(): DependencyInstaller
+    public function testConstructor(): void
     {
-        $installer = new DependencyInstaller(
-            'composer.json',
-            $this->createMock(OutputInterface::class)
-        );
-
         $this->assertInstanceOf(
             DependencyInstaller::class,
-            $installer
+            $this->dependencyInstaller
         );
-
-        return $installer;
     }
 
     /**
-     * @depends      testConstructor
-     * @dataProvider repositoryProvider
-     *
-     * @param string              $name
-     * @param string              $type
-     * @param string              $url
-     * @param DependencyInstaller $dependencyInstaller
-     *
-     * @return void
-     * @covers ::installRepository
+     * @throws ParsingException
      */
+    #[DataProvider('repositoryProvider')]
     public function testInstallRepository(
         string $name,
         string $type,
         string $url,
-        DependencyInstaller $dependencyInstaller
-    ) {
-        $dependencyInstaller->installRepository($name, $type, $url);
+    ): void {
+        $this->dependencyInstaller->installRepository($name, $type, $url);
 
         $jsonFile   = new JsonFile('composer.json');
         $definition = $jsonFile->read();
@@ -121,7 +97,7 @@ class DependencyInstallerTest extends TestCase
     /**
      * @return array
      */
-    public function repositoryProvider(): array
+    public static function repositoryProvider(): array
     {
         return [
             ['mediact', 'composer', 'https://composer.mediact.nl']
@@ -129,27 +105,17 @@ class DependencyInstallerTest extends TestCase
     }
 
     /**
-     * @depends      testConstructor
-     * @dataProvider packageProvider
-     *
-     * @param string $name
-     * @param string $version
-     * @param bool $dev
-     * @param bool $updateDependencies
-     * @param DependencyInstaller $dependencyInstaller
-     *
-     * @return void
      * @throws ParsingException
-     * @covers ::installPackage
      */
+    #[DataProvider('packageProvider')]
     public function testInstallPackage(
         string $name,
         string $version,
         bool $dev,
         bool $updateDependencies,
-        DependencyInstaller $dependencyInstaller
+        bool $allowOverrideVersion
     ) {
-        $dependencyInstaller->installPackage($name, $version, $dev, $updateDependencies);
+        $this->dependencyInstaller->installPackage($name, $version, $dev, $updateDependencies, $allowOverrideVersion);
 
         $jsonFile   = new JsonFile('composer.json');
         $definition = $jsonFile->read();
@@ -161,14 +127,90 @@ class DependencyInstallerTest extends TestCase
         $this->assertEquals($version, $definition[$node][$name]);
     }
 
-    /**
-     * @return array
-     */
-    public function packageProvider(): array
+    public static function packageProvider(): array
     {
         return [
-            ['psr/log', '@stable', true, false],
-            ['psr/log', '@stable', false, false]
+            ['psr/log', '@stable', true, false, true],
+            ['psr/log', '@stable', false, false, true]
         ];
+    }
+
+    /**
+     * @throws ParsingException
+     * @throws Exception
+     */
+    public function testOriginalPackageVersionCanBePreserved() {
+        $this->dependencyInstaller->installPackage(
+            'psr/link',
+            '1.1.1',
+            false,
+            false,
+            false
+        );
+
+        $this->updateComposerFileReference();
+
+        $this->dependencyInstaller->installPackage(
+            'psr/link',
+            '^2',
+            false,
+            false,
+            false
+        );
+
+        $this->updateComposerFileReference();
+
+        $jsonFile   = new JsonFile('composer.json');
+        $definition = $jsonFile->read();
+
+        // Assert the pre-existing composer versions was NOT wiped by a dependency installer package installation
+        $this->assertArrayHasKey('require', $definition);
+        $this->assertArrayHasKey('psr/link', $definition['require']);
+        $this->assertEquals('1.1.1', $definition['require']['psr/link']);
+    }
+
+    /**
+     * @throws ParsingException|Exception
+     */
+    public function testOriginalPackageVersionCanBeOverridden(): void {
+        // Install and validate base package availability
+        $this->dependencyInstaller->installPackage(
+            'psr/link',
+            '1.1.1',
+            false
+        );
+
+        $this->updateComposerFileReference();
+
+        $this->dependencyInstaller->installPackage(
+            'psr/link',
+            '@stable',
+            false
+        );
+
+        $jsonFile   = new JsonFile('composer.json');
+        $definition = $jsonFile->read();
+
+        $this->assertArrayHasKey('require', $definition);
+        $this->assertArrayHasKey('psr/link', $definition['require']);
+        $this->assertEquals('@stable', $definition['require']['psr/link']);
+    }
+
+    /**
+     * Update the composer file reference since it is loaded only once as part of the constructor.
+     *
+     * We refresh the composer file reference whenever the composer file is changed, otherwise the dependency
+     * installer will always hold a reference to the previous state of the composer file and not contain any
+     * changes if multiple installations happen in the same testing function.
+     *
+     * @return void
+     * @throws Exception
+     */
+    private function updateComposerFileReference(): void
+    {
+        $this->dependencyInstaller = new DependencyInstaller(
+            'composer.json',
+            $this->createMock(OutputInterface::class)
+        );
     }
 }
